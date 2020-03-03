acc4ac86a0f9783973198fc950a11e801c2d3065
RY4121 <yoshidarariru@gmail.com>
2020-03-04 00:34:15 +0900

    change main.py

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ import os
 
 app = Flask(__name__)
 
-#環境変数取得
+# 環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,16 +41,32 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='うんこ漏れそう'))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text='うんこ漏れそう'))
         # text=event.message.text)
+        profile = line_bot_api.get_profile(event.source.user_id)
+
+    status_msg = profile.status_message
+    if status_msg != "None":
+        # LINEに登録されているstatus_messageが空の場合は、"なし"という文字列を代わりの値とする
+        status_msg = "なし"
+
+    messages = TemplateSendMessage(alt_text="Buttons template",
+                                   template=ButtonsTemplate(
+                                       thumbnail_image_url=profile.picture_url,
+                                       title=profile.display_name,
+                                       text=f"User Id: {profile.user_id[:5]}...\n"
+                                            f"Status Message: {status_msg}",
+                                       actions=[MessageAction(label="成功", text="次は何を実装しましょうか？")]))
+
+    line_bot_api.reply_message(event.reply_token, messages=messages)
 
 
 if __name__ == "__main__":
 #    app.run()
-#追加しましたby YUTO
-#追加しましたby Ryoga
-#追加しました2by YUTO
+# 追加しましたby YUTO
+# 追加しましたby Ryoga
+# 追加しました2by YUTO
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
