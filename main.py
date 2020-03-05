9be85de2ea39a0844e921739ae3e7ff5742d2fb0
RY4121 <yoshidarariru@gmail.com>
2020-03-05 22:14:17 +0900

    text add

--- main.py
+++ main.py
@@ -39,20 +39,36 @@ def callback():
 
     return 'OK'
 
+
 # スタンプが送られてきた時の処理
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        StickerSendMessage(package_id=event.message.package_id, sticker_id=event.message.sticker_id))
+    try:
+        line_bot_api.reply_message(
+            event.reply_token,
+            StickerSendMessage(package_id=event.message.package_id, sticker_id=event.message.sticker_id))
+    except Exception as e:
+        print("error:", e)
+        reply_message(
+            event, TextSendMessage(text=str(e))
+        )
+
 
 # メッセージが送られてきたときの処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # wget.getStoreInfo(event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='うんこ漏れそう'))
+    try:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='うんこ漏れそう'))
+        reply_message(event, messages)
+    except Exception as e:
+        print("error:", e)
+        reply_message(
+            event, TextSendMessage(text=str(e))
+        )
+
 
 # 画像が送られきた時の処理
 @handler.add(MessageEvent, message=ImageMessage)
@@ -65,19 +81,21 @@ def handle_image(event):
 
     try:
         # result=search_product(image)
-
         messages = [
-            TextSendMessage(text='result'),
-            TextSendMessage(text='食品パッケージ/書籍/CD/DVD/ゲームソフト/PCソフトを検索できるよ！')
+            TextSendMessage(text='画像が送信されました')
+            # TextSendMessage(text='食品パッケージ/書籍/CD/DVD/ゲームソフト/PCソフトを検索できるよ！')
         ]
 
         reply_message(event, messages)
 
     except Exception as e:
         print("error:", e)
-        reply_message(event, TextSendMessage(text='エラーが発生しました'))
+        reply_message(
+            event, TextSendMessage(text=str(e))
+        )
 
 
+# LineBotApiから呼び出さない。自作
 def reply_message(event, messages):
     line_bot_api.reply_message(
         event.reply_token,
