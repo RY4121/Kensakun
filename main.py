b94b720bf5ac91a48a5a4fd76d939e558f336f90
RY4121 <yoshidarariru@gmail.com>
2020-03-05 20:23:46 +0900

    change func

--- main.py
+++ main.py
@@ -41,17 +41,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    wget.getStoreInfo(event.message.text)
+    # wget.getStoreInfo(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='うんこ漏れそう'))
     # text=event.message.text)
 
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(package_id=event.message.package_id, sticker_id=event.message.sticker_id))
+
 
 if __name__ == "__main__":
     #    app.run()
-    # 追加しましたby YUTO
-    # 追加しましたby Ryoga
-    # 追加しました2by YUTO
+    # 追加しましたby Ryoga 2020/3/5
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
